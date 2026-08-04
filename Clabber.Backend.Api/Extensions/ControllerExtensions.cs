0c429501d9220940f4f1e07a76d5e4c1ca628b67
Armaziko <luka80994@gmail.com>
2026-08-04 16:42:38 +0400

    Separate logic of returning action results based on resuult objects in an extension.

--- Clabber.Backend.Api/Extensions/ControllerExtensions.cs
+++ Clabber.Backend.Api/Extensions/ControllerExtensions.cs
@@ -10,9 +10,34 @@ namespace Clabber.Backend.Api.Extensions
             this ControllerBase controller,
             Result result)
         {
-            return controller.StatusCode(
-                ResultMapper.GetHttpStatusCode(result.StatusCode),
-                result);
+            if (result is null)
+            {
+                return controller.StatusCode(500);
+            }
+
+            var type = result.GetType();
+            var valueInfo = type.GetProperty("Value");
+
+            if (!type.IsGenericType || type.GetGenericTypeDefinition() != typeof(Result<>) || valueInfo is null)
+            {
+                if (result.Messages != null && result.Messages.Count > 0)
+                {
+                    return controller.StatusCode(
+                        ResultMapper.GetHttpStatusCode(result.StatusCode), new { messages = result.Messages });
+                }
+                return controller.StatusCode(
+                    ResultMapper.GetHttpStatusCode(result.StatusCode));
+            }
+
+            var value = valueInfo.GetValue(result);
+
+            // In case value property is null, return messages as they might hold data about why it was null.
+            if (value is null && result.Messages != null && result.Messages.Count > 0)
+            {
+                return controller.StatusCode(ResultMapper.GetHttpStatusCode(result.StatusCode), new { messages = result.Messages });
+            }
+
+            return controller.StatusCode(ResultMapper.GetHttpStatusCode(result.StatusCode), value);
         }
     }
 }
