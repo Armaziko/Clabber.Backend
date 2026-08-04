e6afa27eb472b500f7514e660ae5e006cf7c9b48
Armaziko <luka80994@gmail.com>
2026-08-04 16:40:48 +0400

    Relocate AccountDto to RequestDTOs

--- Clabber.Backend.Application/DTOs/Account/AccountDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Clabber.Backend.Application.DTOs.Account
-{
-    public class AccountDto
-    {
-        public Guid Id { get; set; }
-        public string DisplayName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
