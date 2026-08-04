3acec4d063b849f65da657dec894a7a6721974a0
Armaziko <luka80994@gmail.com>
2026-08-04 15:33:45 +0400

    Add a separte directory for incoming dtos.

---
+++ Clabber.Backend.Application/DTOs/RequestDTOs/Account/CompleteUpdateAccountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Clabber.Backend.Application.DTOs.RequestDTOs.Account
+{
+    public class CompleteUpdateAccountDto
+    {
+        public Guid Id { get; set; }
+        public string DisplayName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Mail { get; set; } = string.Empty;
+    }
+}
---
+++ Clabber.Backend.Application/DTOs/RequestDTOs/Account/CreateAccountDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Clabber.Backend.Application.DTOs.RequestDTOs.Account
+{
+    public class CreateAccountDto
+    {
+        public string DisplayName { get; set; } = string.Empty;
+        public string Mail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
