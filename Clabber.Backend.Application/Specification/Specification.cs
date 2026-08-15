76cadd158dc1c34e0875ed91e4fb4d31a02ee4a5
Armaziko <luka80994@gmail.com>
2026-08-15 13:00:58 +0400

    Feature: Add base specification pattern to allow query modification

---
+++ Clabber.Backend.Application/Specification/Specification.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace Clabber.Backend.Application.Specification
+{
+    public class Specification<T>
+    {
+        public List<Expression<Func<T, object>>> Includes { get; }
+
+        public Expression<Func<T, bool>>? Criteria { get; }
+
+        public bool AsNoTracking { get; private set; } = false;
+
+        public Specification(Expression<Func<T, bool>>? criteria = null)
+        {
+            this.Criteria = criteria;
+            this.Includes = [];
+        }
+
+        public void AddInclude(Expression<Func<T, object>> include)
+        {
+            this.Includes.Add(include);
+        }
+
+        public void AsNoTrackingQuery()
+        {
+            this.AsNoTracking = true;
+        }
+    }
+}
