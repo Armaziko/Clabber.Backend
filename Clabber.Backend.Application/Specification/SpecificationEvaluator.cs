63e67fb0ce130a45d61568cbe05478b5526f031a
Armaziko <luka80994@gmail.com>
2026-08-15 13:01:55 +0400

    Feature: Add SpecificationEvaluator.cs, which has a static function that allows to create a new query from a base query and a specification object.

---
+++ Clabber.Backend.Application/Specification/SpecificationEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clabber.Backend.Application.Specification
+{
+    public static class SpecificationEvaluator
+    {
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> query, Specification<T> specification)
+            where T : class
+        {
+            if (specification.AsNoTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (specification.Criteria is not null)
+            {
+                query = query.Where(specification.Criteria);
+            }
+
+            if (specification.Includes != null && specification.Includes.Count != 0)
+            {
+                foreach (var include in specification.Includes) 
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return query;
+        }
+    }
+}
