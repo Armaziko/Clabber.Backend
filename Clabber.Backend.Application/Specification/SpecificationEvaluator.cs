6f89f09da97b63d52e19005cef98e7b96ad79cd6
Armaziko <luka80994@gmail.com>
2026-08-18 18:27:36 +0400

    Make GetQuery method accept an implementation of ISpecification pattern instead of a hard-coded implementation.

--- Clabber.Backend.Application/Specification/SpecificationEvaluator.cs
+++ Clabber.Backend.Application/Specification/SpecificationEvaluator.cs
@@ -1,21 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Clabber.Backend.Application.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clabber.Backend.Application.Specification
 {
     public static class SpecificationEvaluator
     {
-        public static IQueryable<T> GetQuery<T>(IQueryable<T> query, Specification<T> specification)
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> query, ISpecification<T>? specification = null)
             where T : class
         {
+            if (specification is null)
+                return query;
+
             if (specification.AsNoTracking)
-            {
                 query = query.AsNoTracking();
-            }
 
             if (specification.Criteria is not null)
-            {
                 query = query.Where(specification.Criteria);
-            }
 
             if (specification.Includes != null && specification.Includes.Count != 0)
             {
