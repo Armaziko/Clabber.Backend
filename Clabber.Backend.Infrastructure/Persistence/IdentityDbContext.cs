f0d9a2a6e49f69111fb04eb82e94e656227d69a7
Armaziko <luka80994@gmail.com>
2026-08-22 19:43:54 +0400

    Feat: Add a separate context that uses Identity.

---
+++ Clabber.Backend.Infrastructure/Persistence/IdentityDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Clabber.Backend.Domain.Entities.Profile;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clabber.Backend.Infrastructure.Persistence
+{
+    public class IdentityDbContext : IdentityDbContext<Account, IdentityRole<Guid>, Guid>
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Verification> Verifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Apply Account and Verification configurations in the identity model
+            modelBuilder.ApplyConfiguration(new Configurations.AccountConfiguration());
+            modelBuilder.ApplyConfiguration(new Configurations.VerificationConfiguration());
+        }
+    }
+}
