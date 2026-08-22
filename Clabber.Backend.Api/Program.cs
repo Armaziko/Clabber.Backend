4fec9e6ddf3ef55de97987480317a83e4667955c
Armaziko <luka80994@gmail.com>
2026-08-22 19:46:33 +0400

    Make sure the infrastructure and api are set up in correct order and that the data is seeded while in development.

--- Clabber.Backend.Api/Program.cs
+++ Clabber.Backend.Api/Program.cs
@@ -1,15 +1,17 @@
 using Clabber.Backend.Api;
 using Clabber.Backend.Application;
 using Clabber.Backend.Infrastructure;
+using Clabber.Backend.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 
+// First Infrastructure than Api is mandatory to ensure identity's context uses dev-specific connection string
+builder.SetUpInfrastructure();
 builder.SetupApi();
 builder.SetUpApplication();
-builder.SetUpInfrastructure();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +22,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    // Run development data seeder
+    using (var scope = app.Services.CreateScope())
+    {
+        var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+        await seeder.SeedAsync();
+    }
 }
 
 app.UseCors();
