d7943bcccce5c0e6a6fff536f253d460c6ddc537
Armaziko <luka80994@gmail.com>
2026-08-22 20:09:47 +0400

    Feat: Add logic to switch between SqlServer and SqlLite during development

--- Clabber.Backend.Infrastructure/Extensions/ContextExtension.cs
+++ Clabber.Backend.Infrastructure/Extensions/ContextExtension.cs
@@ -10,12 +10,34 @@ namespace Clabber.Backend.Infrastructure.Extensions
     {
         public static void SetUpContext(this IHostApplicationBuilder builder)
         {
-            var connectionStrings = builder.Configuration.GetConnectionString("SqlServer_01") ?? throw new ArgumentException("Connection string is null.");
-
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            if (builder.Environment.IsDevelopment())
             {
-                options.UseSqlServer(connectionStrings);
-            });
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlite("Data Source=dev_app.db");
+                });
+
+                builder.Services.AddDbContext<IdentityDbContext>(options =>
+                {
+                    options.UseSqlite("Data Source=dev_identity.db");
+                });
+            }
+            else
+            {
+                var appConnectionStrings = builder.Configuration.GetConnectionString("Application_SqlServer_01") ?? throw new ArgumentException("Connection string is null.");
+                var identityConnectionStrings = builder.Configuration.GetConnectionString("Identity_SqlServer_01") ?? throw new ArgumentException("Connection string is null.");
+
+
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(appConnectionStrings);
+                });
+
+                builder.Services.AddDbContext<IdentityDbContext>(options =>
+                {
+                    options.UseSqlServer(identityConnectionStrings);
+                });
+            }
         }
     }
 }
