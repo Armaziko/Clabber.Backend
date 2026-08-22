f96f3f59b48bd8597e5e60b2a82d9efa5d03c1e4
Armaziko <luka80994@gmail.com>
2026-08-22 19:30:41 +0400

    Fix: Remove redundant IsDeleted as I added it to the BaseEntity.

--- Clabber.Backend.Domain/Entities/Media/MediaCollection.cs
+++ Clabber.Backend.Domain/Entities/Media/MediaCollection.cs
@@ -10,8 +10,6 @@ namespace Clabber.Backend.Domain.Entities.Media
 
         public Guid OwnerId { get; set; }
 
-        public bool IsDeleted { get; set; }
-
         // Navigation properties
 
         public ICollection<MediaCollectionItem> MediaCollectionItems { get; set; } = new List<MediaCollectionItem>();
