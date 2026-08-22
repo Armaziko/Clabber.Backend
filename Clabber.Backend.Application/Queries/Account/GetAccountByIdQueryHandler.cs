b91c1a7d52f24d871cf3508637faf8ece988bb08
Armaziko <luka80994@gmail.com>
2026-08-22 19:41:20 +0400

    Feat: Implement default behaviour for getting an account by query using UserManager.

--- Clabber.Backend.Application/Queries/Account/GetAccountByIdQueryHandler.cs
+++ Clabber.Backend.Application/Queries/Account/GetAccountByIdQueryHandler.cs
@@ -1,14 +1,35 @@
-﻿using Clabber.Backend.Application.DTOs.ResponseDTOs.Account;
+﻿using Clabber.Backend.Application.Abstractions;
+using Clabber.Backend.Application.DTOs.ResponseDTOs.Account;
 using Clabber.Backend.Application.Results;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using DomainAccount = Clabber.Backend.Domain.Entities.Profile.Account;
 
 namespace Clabber.Backend.Application.Queries.Account
 {
     public class GetAccountByIdQueryHandler : IRequestHandler<GetAccountByIdQuery, Result<AccountDto>>
     {
-        public Task<Result<AccountDto>> Handle(GetAccountByIdQuery request, CancellationToken cancellationToken)
+        private readonly UserManager<DomainAccount> userManager;
+        public GetAccountByIdQueryHandler(UserManager<DomainAccount> userManager)
         {
-            throw new NotImplementedException();
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+        public async Task<Result<AccountDto>> Handle(GetAccountByIdQuery request, CancellationToken cancellationToken)
+        {
+
+            var user = await userManager.FindByIdAsync(request.Id.ToString());
+
+            if (user is null)
+            {
+                return Result<AccountDto>.NotFound();
+            }
+
+            return Result<AccountDto>.Success(new AccountDto() 
+                { 
+                    Id = user.Id,
+                    DisplayName = user.DisplayName,
+                    Email = !string.IsNullOrWhiteSpace(user.Email) ? user.Email : "anonymous@nomail.com" 
+                });
         }
     }
 }
