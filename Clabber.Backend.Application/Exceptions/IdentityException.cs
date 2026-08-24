c9d8608a6b8cd8602b5157494ab631f788c511ac
Armaziko <luka80994@gmail.com>
2026-08-24 19:42:42 +0400

    Add an alias for Account entity class and move the Command and Query directiories into CQRS directory.

---
+++ Clabber.Backend.Application/Exceptions/IdentityException.cs
@@ -0,0 +1,9 @@
+﻿namespace Clabber.Backend.Application.Exceptions
+{
+    public class IdentityException : Exception
+    {
+        public IdentityException(string message)
+            : base(message)
+        { }
+    }
+}
