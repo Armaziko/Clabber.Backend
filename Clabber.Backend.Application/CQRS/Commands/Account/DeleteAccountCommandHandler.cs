f7d65595d029420e25e9716ed612bf323f8cee68
Armaziko <luka80994@gmail.com>
2026-08-28 16:34:21 +0400

    Implement an authorization-less logic orchestration for deleting an account.

--- Clabber.Backend.Application/CQRS/Commands/Account/DeleteAccountCommandHandler.cs
+++ Clabber.Backend.Application/CQRS/Commands/Account/DeleteAccountCommandHandler.cs
@@ -1,13 +1,44 @@
 ﻿using Clabber.Backend.Application.Results;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using DomainAccount = Clabber.Backend.Domain.Entities.Profile.Account;
 
 namespace Clabber.Backend.Application.CQRS.Commands.Account
 {
     public class DeleteAccountCommandHandler : IRequestHandler<DeleteAccountCommand, Result>
     {
-        public Task<Result> Handle(DeleteAccountCommand request, CancellationToken cancellationToken)
+        private readonly UserManager<DomainAccount> userManager;
+        private readonly ILogger<DeleteAccountCommandHandler> logger;
+        public DeleteAccountCommandHandler(UserManager<DomainAccount> userManager, ILogger<DeleteAccountCommandHandler> logger)
         {
-            throw new NotImplementedException();
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public async Task<Result> Handle(DeleteAccountCommand request, CancellationToken cancellationToken)
+        {
+            var user = await this.userManager.FindByIdAsync(request.Id.ToString());
+            if (user == null) 
+            {
+                return Result.NotFound();
+            }
+
+            try
+            {
+                var attemptDelete = await this.userManager.DeleteAsync(user);
+                if (!attemptDelete.Succeeded)
+                {
+                    return Result.Failed();
+                }
+
+                return Result.Success();
+            }
+            catch(DbUpdateException dbException)
+            {
+                this.logger.LogError(dbException, "An exception has occured while attempting to delete an user: {Message}", dbException.Message);
+                return Result.Failed();
+            } 
         }
     }
 }
