59c624736d9cc86500384afdad92b67c8e128081
Armaziko <luka80994@gmail.com>
2026-08-28 16:37:48 +0400

    Update namespace

--- Clabber.Backend.Application/CQRS/Commands/Account/CreateAccountCommand.cs
+++ Clabber.Backend.Application/CQRS/Commands/Account/CreateAccountCommand.cs
@@ -2,7 +2,7 @@
 using Clabber.Backend.Application.Results;
 using MediatR;
 
-namespace Clabber.Backend.Application.Commands.Account
+namespace Clabber.Backend.Application.CQRS.Commands.Account
 {
     public record CreateAccountCommand(CreateAccountDto Model) : IRequest<Result>;
 }
