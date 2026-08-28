d55f1d75888892299ade1864d39aa90ef267fac6
Armaziko <luka80994@gmail.com>
2026-08-28 16:39:35 +0400

    Relocate Queries to the correct position.

---
+++ Clabber.Backend.Application/CQRS/Queries/Account/GetAccountByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Clabber.Backend.Application.DTOs.ResponseDTOs.Account;
+using Clabber.Backend.Application.Results;
+using MediatR;
+
+namespace Clabber.Backend.Application.CQRS.Queries.Account
+{
+    public class GetAccountByIdQuery : IRequest<Result<AccountDto>>
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+    }
+}
---
+++ Clabber.Backend.Application/CQRS/Queries/Account/GetAccountByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Clabber.Backend.Application.Abstractions;
+using Clabber.Backend.Application.DTOs.ResponseDTOs.Account;
+using Clabber.Backend.Application.Results;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using DomainAccount = Clabber.Backend.Domain.Entities.Profile.Account;
+
+namespace Clabber.Backend.Application.CQRS.Queries.Account
+{
+    public class GetAccountByIdQueryHandler : IRequestHandler<GetAccountByIdQuery, Result<AccountDto>>
+    {
+        private readonly UserManager<DomainAccount> userManager;
+        public GetAccountByIdQueryHandler(UserManager<DomainAccount> userManager)
+        {
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+        public async Task<Result<AccountDto>> Handle(GetAccountByIdQuery request, CancellationToken cancellationToken)
+        {
+
+            var user = await userManager.FindByIdAsync(request.Id.ToString());
+
+            if (user is null)
+            {
+                return Result<AccountDto>.NotFound();
+            }
+
+            return Result<AccountDto>.Success(new AccountDto() 
+                { 
+                    Id = user.Id,
+                    DisplayName = user.DisplayName,
+                    Email = !string.IsNullOrWhiteSpace(user.Email) ? user.Email : "anonymous@nomail.com" 
+                });
+        }
+    }
+}
