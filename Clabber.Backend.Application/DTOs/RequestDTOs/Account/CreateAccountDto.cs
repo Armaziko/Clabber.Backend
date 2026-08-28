14c5f027147d750eb8be35ec9444b035a17a8677
Armaziko <luka80994@gmail.com>
2026-08-28 16:38:35 +0400

    Add UserName property.

--- Clabber.Backend.Application/DTOs/RequestDTOs/Account/CreateAccountDto.cs
+++ Clabber.Backend.Application/DTOs/RequestDTOs/Account/CreateAccountDto.cs
@@ -3,6 +3,7 @@
     public class CreateAccountDto
     {
         public string DisplayName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
         public string Mail { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
