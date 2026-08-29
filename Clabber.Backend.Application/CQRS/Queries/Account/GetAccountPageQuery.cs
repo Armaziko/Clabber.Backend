25020341265b67dd28ec08c5c0385af8a15bc073
Armaziko <luka80994@gmail.com>
2026-08-29 16:08:30 +0400

    Add default value to ElementPerPage.

--- Clabber.Backend.Application/CQRS/Queries/Account/GetAccountPageQuery.cs
+++ Clabber.Backend.Application/CQRS/Queries/Account/GetAccountPageQuery.cs
@@ -6,8 +6,8 @@ namespace Clabber.Backend.Application.CQRS.Queries.Account
 {
     public class GetAccountPageQuery : IRequest<Result<IReadOnlyList<AccountDto>>>
     {
-        public int Page { get; set; }
-        public int ElementsPerPage { get; set; }
+        public int Page { get; set; } = 0;
+        public int ElementsPerPage { get; set; } = 8;
         public bool? OnlyCreators { get; set; }
         public bool? OnlySponsors { get; set; }
         public bool? OnlyVerified { get; set; }
