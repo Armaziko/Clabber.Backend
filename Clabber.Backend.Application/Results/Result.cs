0433682893474e99befd998978de7e179ce65d33
Armaziko <luka80994@gmail.com>
2026-08-29 16:09:40 +0400

    Improve efficiency by adding a constructor that doesn't take a message value and make Messages have a default list value. Make success functions initialize result objects without messages passed to their constructors.

--- Clabber.Backend.Application/Results/Result.cs
+++ Clabber.Backend.Application/Results/Result.cs
@@ -5,6 +5,11 @@
     /// </summary>
     public class Result
     {
+        public Result(bool isSuccess, OperationStatusCode statusCode)
+        {
+            this.IsSuccess = isSuccess;
+            this.StatusCode = statusCode;
+        }
         public Result(bool isSuccess, List<string> messages, OperationStatusCode statusCode)
         {
             this.IsSuccess = isSuccess;
@@ -24,12 +29,12 @@
         /// <summary>
         /// Gets or sets the collection of messages associated with the current instance.
         /// </summary>
-        public List<string> Messages { get; set; }
+        public List<string> Messages { get; set; } = [];
         /// <summary>
         /// Gets or sets the result code that indicates the outcome of the operation.
         /// </summary>
         public OperationStatusCode StatusCode { get; set; }
-        public static Result Success() => new Result(true, "Operation was successful.", OperationStatusCode.SUCCESS);
+        public static Result Success() => new Result(true, OperationStatusCode.SUCCESS);
         public static Result Failed() => new Result(false, "Operation was a failure.", OperationStatusCode.FAILED);
         public static Result Failed(string message) => new Result(false, message, OperationStatusCode.FAILED);
         public static Result Failed(List<string> messages) => new Result(false, messages, OperationStatusCode.FAILED);
@@ -44,6 +49,10 @@
     /// </summary>
     public class Result<T> : Result
     {
+        public Result(bool isSuccess, OperationStatusCode statusCode, T? value) : base(isSuccess, statusCode)
+        {
+            this.Value = value;
+        }
         public Result(bool isSuccess, List<string> messages, OperationStatusCode statusCode, T? value) : base(isSuccess, messages, statusCode)
         {
             this.Value = value;
@@ -54,7 +63,7 @@
         }
 
         public T? Value { get; set; }
-        public static Result<T> Success(T value) => new Result<T>(true, "Operation was successful.", OperationStatusCode.SUCCESS, value);
+        public static Result<T> Success(T value) => new Result<T>(true, OperationStatusCode.SUCCESS, value);
         public new static Result<T> Failed() => new Result<T>(false, "Operation was a failure.", OperationStatusCode.FAILED, default);
         public new static Result<T> Failed(string message) => new Result<T>(false, message, OperationStatusCode.FAILED, default);
         public new static Result<T> Failed(List<string> messages) => new Result<T>(false, messages, OperationStatusCode.FAILED, default);
