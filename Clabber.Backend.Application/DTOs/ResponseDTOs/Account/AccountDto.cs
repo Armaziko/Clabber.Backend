2ae3d5455fc8fddcbd98ec33c7e511696a389a27
Armaziko <luka80994@gmail.com>
2026-08-30 00:11:09 +0400

    Add IsVerified property to the AccountDto

--- Clabber.Backend.Application/DTOs/ResponseDTOs/Account/AccountDto.cs
+++ Clabber.Backend.Application/DTOs/ResponseDTOs/Account/AccountDto.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; }
         public string DisplayName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public bool IsVerified { get; set; } = false;
     }
 }
