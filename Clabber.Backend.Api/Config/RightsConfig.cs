74e9f1f67a8edc9df43efae814291b3f2703e757
Armaziko <luka80994@gmail.com>
2026-09-06 19:16:01 +0400

    Add a model for roles.

---
+++ Clabber.Backend.Api/Config/RightsConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Clabber.Backend.Api.Config
+{
+    public class RightsConfig
+    {
+        public static readonly string AdminRightsPolicyName = "AdminRights";
+        public static readonly string CreatorRightsPolicyName = "CreatorRights";
+        public static readonly string SponsorRightsPolicyName = "SponsorRights";
+        public List<string> CreatorRights { get; set; } = new();
+        public List<string> SponsorRights { get; set; } = new();
+        public List<string> AdminRights { get; set; } = new();
+    }
+}
