07df5c4c02ed184f3de0edb6d173192d0e46959c
Armaziko <luka80994@gmail.com>
2026-09-06 19:15:11 +0400

    Add Authorization middleware that adds Role based policies

---
+++ Clabber.Backend.Api/Extensions/AuthhorizationExtension.cs
@@ -0,0 +1,27 @@
+﻿using Clabber.Backend.Api.Config;
+
+namespace Clabber.Backend.Api.Extensions
+{
+    public static class AuthhorizationExtension
+    {
+        public static void SetUpAuthorization(this IHostApplicationBuilder builder)
+        {
+            var config = builder.Configuration.GetSection("AuthDetails:Rights").Get<RightsConfig>();
+            if (config is null)
+            {
+                throw new InvalidOperationException("RightsConfig object couldn't be created using AuthDetails:Rights.");
+            }
+            builder.Services.AddAuthorizationBuilder()
+                .AddPolicy(RightsConfig.AdminRightsPolicyName, policy =>
+                {
+                    policy.RequireRole(config.AdminRights);
+                }).AddPolicy(RightsConfig.SponsorRightsPolicyName, policy =>
+                {
+                    policy.RequireRole(config.SponsorRights);
+                }).AddPolicy(RightsConfig.CreatorRightsPolicyName, policy =>
+                {
+                    policy.RequireRole(RightsConfig.CreatorRightsPolicyName);
+                });
+        }
+    }
+}
