c3a858ba07e3d81066d9376bfde772e32bbeb67f
Armaziko <luka80994@gmail.com>
2026-09-06 19:16:28 +0400

    Add option model for authentication details

---
+++ Clabber.Backend.Api/Options/AuthOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Clabber.Backend.Api.Options
+{
+    public record AuthOptions
+    {
+        public static string NameTitle { get; } = "AuthDetails";
+        public string ValidIssuer { get; set; } = default!;
+        public string ValidAudience { get; set; } = default!;
+        public string IssuerSigningKey { get; set; } = default!;
+        public string AuthCookieName { get; set; } = default!;
+    }
+}
