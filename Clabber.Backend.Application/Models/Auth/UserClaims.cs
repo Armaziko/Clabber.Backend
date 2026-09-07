34d20bafd9d2f49aa3bd04c4e53245f1998c0bb5
Armaziko <luka80994@gmail.com>
2026-09-07 14:41:11 +0400

    Add UserClaims model, that represents claims about the user.

---
+++ Clabber.Backend.Application/Models/Auth/UserClaims.cs
@@ -0,0 +1,8 @@
+﻿namespace Clabber.Backend.Application.Models.Auth
+{
+    public class UserClaims
+    {
+        public Guid Id { get; set; }
+        public string Role { get; set; } = string.Empty;
+    }
+}
