4918edad2763d58dea46521b67718c2e4953f66d
Armaziko <luka80994@gmail.com>
2026-09-07 14:40:41 +0400

    Add interface for generating a bearer token.

---
+++ Clabber.Backend.Application/Abstractions/IBearerTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Clabber.Backend.Application.Models.Auth;
+
+namespace Clabber.Backend.Application.Abstractions
+{
+    public interface IBearerTokenGenerator
+    {
+        public string GetBearerToken(UserClaims userClaims);
+    }
+}
