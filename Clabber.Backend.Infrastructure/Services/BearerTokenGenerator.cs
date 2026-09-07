549734cfcce88404dba40283b33c076e967bc227
Armaziko <luka80994@gmail.com>
2026-09-07 14:42:51 +0400

    Implement IBearerTokenGenerator. Use dependency injection and options pattern to populate AuthBearerTokenConfig class. Use AuthBearerTokenConfig object and UserClaims object to create a valid jwt string.

---
+++ Clabber.Backend.Infrastructure/Services/BearerTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Clabber.Backend.Application.Abstractions;
+using Clabber.Backend.Application.Models.Auth;
+using Clabber.Backend.Infrastructure.Config;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Clabber.Backend.Infrastructure.Services
+{
+    public class BearerTokenGenerator : IBearerTokenGenerator
+    {
+        private readonly AuthBearerTokenConfig tokenConfig;
+        public BearerTokenGenerator(IOptions<AuthBearerTokenConfig> authOptions)
+        {
+            tokenConfig = authOptions.Value;
+        }
+        public string GetBearerToken(UserClaims userClaims)
+        {
+            var claims = new[]{
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Sub, userClaims.Id.ToString()),
+                new Claim(ClaimTypes.Role, userClaims.Role)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfig.IssuerSigningKey));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.Sha256);
+
+            var jwt = new JwtSecurityToken(
+                issuer: tokenConfig.ValidIssuer, 
+                audience: tokenConfig.ValidAudience, 
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(tokenConfig.TokenLifetimeInMinutes),
+                signingCredentials: credentials);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+    }
+}
