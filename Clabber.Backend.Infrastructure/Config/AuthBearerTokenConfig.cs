b2dfb1586c85a2eb6cd268cbe093a5d87ad46296
Armaziko <luka80994@gmail.com>
2026-09-07 14:43:20 +0400

    Migrate Config from .Api layer to .Infrastructure to better follow clear architecture rules.

---
+++ Clabber.Backend.Infrastructure/Config/AuthBearerTokenConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Clabber.Backend.Infrastructure.Config
+{
+    public record AuthBearerTokenConfig
+    {
+        public static string NameTitle { get; } = "AuthBearerToken";
+        public string ValidIssuer { get; set; } = default!;
+        public string ValidAudience { get; set; } = default!;
+        public string IssuerSigningKey { get; set; } = default!;
+        public string AuthCookieName { get; set; } = default!;
+        public int TokenLifetimeInMinutes { get; set; }
+    }
+}
