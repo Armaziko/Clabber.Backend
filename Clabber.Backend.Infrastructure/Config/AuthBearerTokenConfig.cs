7d0100557fb5ecfdb0d1109b4ade1dfa332a0f95
Armaziko <luka80994@gmail.com>
2026-09-08 15:32:02 +0400

    Change property name from NameTitle to SectionName.

--- Clabber.Backend.Infrastructure/Config/AuthBearerTokenConfig.cs
+++ Clabber.Backend.Infrastructure/Config/AuthBearerTokenConfig.cs
@@ -2,7 +2,7 @@
 {
     public record AuthBearerTokenConfig
     {
-        public static string NameTitle { get; } = "AuthBearerToken";
+        public static string SectionName { get; } = "AuthBearerToken";
         public string ValidIssuer { get; set; } = default!;
         public string ValidAudience { get; set; } = default!;
         public string IssuerSigningKey { get; set; } = default!;
