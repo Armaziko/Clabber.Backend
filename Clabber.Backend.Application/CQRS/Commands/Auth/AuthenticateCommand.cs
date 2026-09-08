21d717e529b5c19bd01c6d1757da89038cb64584
Armaziko <luka80994@gmail.com>
2026-09-08 15:14:23 +0400

    Create a command model for authentication purposes.

---
+++ Clabber.Backend.Application/CQRS/Commands/Auth/AuthenticateCommand.cs
@@ -0,0 +1,8 @@
+﻿using Clabber.Backend.Application.DTOs.RequestDTOs.Auth;
+using Clabber.Backend.Application.Results;
+using MediatR;
+
+namespace Clabber.Backend.Application.CQRS.Commands.Auth
+{
+    public record AuthenticateCommand(AuthenticateDto AuthDto) : IRequest<Result<string>>;
+}
