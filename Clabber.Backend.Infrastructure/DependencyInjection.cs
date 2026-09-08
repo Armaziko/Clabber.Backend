9dad000e8839c74671a7d43f1e60a660dadcb85f
Armaziko <luka80994@gmail.com>
2026-09-08 15:32:44 +0400

    Register IBearerTokenGenerator and its implementation as scoped service and add an options service for AuthBearerTokenConfig

--- Clabber.Backend.Infrastructure/DependencyInjection.cs
+++ Clabber.Backend.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Clabber.Backend.Application.Abstractions;
+using Clabber.Backend.Infrastructure.Config;
 using Clabber.Backend.Infrastructure.Extensions;
 using Clabber.Backend.Infrastructure.Persistence;
+using Clabber.Backend.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,8 +12,10 @@ namespace Clabber.Backend.Infrastructure
     {
         public static void SetUpInfrastructure(this IHostApplicationBuilder builder)
         {
+            builder.Services.AddOptions<AuthBearerTokenConfig>().BindConfiguration(AuthBearerTokenConfig.SectionName);
             builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IBearerTokenGenerator, BearerTokenGenerator>();
             builder.Services.AddTransient<DataSeeder>();
 
             builder.SetUpContext();
