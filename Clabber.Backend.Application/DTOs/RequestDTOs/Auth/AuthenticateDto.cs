9f5e4cd37aa592adf08bfffd0060bb31234e9c73
Armaziko <luka80994@gmail.com>
2026-09-09 15:21:32 +0400

    Add dto for holding data needed for user authentication.

---
+++ Clabber.Backend.Application/DTOs/RequestDTOs/Auth/AuthenticateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Clabber.Backend.Application.DTOs.RequestDTOs.Auth
+{
+    public class AuthenticateDto
+    {
+        public string UserNameOrEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
